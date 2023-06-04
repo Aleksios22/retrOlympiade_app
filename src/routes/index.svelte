46c54902271be3427719030a8ca368e943395b14
Aleksios22 <alexisrochon98@outlook.com>
2023-06-04 16:55:51 -0400

    1 file modified

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,10 +4,15 @@
 
 <main>
 <h1>64-bit Games</h1>
-
 <button on:click={() => games = []}>Clear List</button>
   <button on:click={() => games = gamesData['64bit']['genre'].racing.games}>Racing</button>
   <button on:click={() => games = gamesData['64bit']['genre'].sport.games}>Sport</button>
+
+    <ul>
+    {#each games as game}
+      <li>{game.title} - {game.platform}</li>
+    {/each}
+  </ul>
 
 
 </main>
