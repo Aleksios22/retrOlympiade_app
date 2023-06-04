c49bdb2dcdfe90c11560d755358f9f1872e94058
Aleksios22 <alexisrochon98@outlook.com>
2023-06-04 16:58:45 -0400

    1 file modified, 11 added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,14 @@
 <script>
   import gamesData from '../data/games.json';
-  let games = gamesData['64bit']['genre'];</script>
-
+  let games = Object.values(gamesData['64bit']['genre']).flatMap(genre => genre.games);
 <main>
 <h1>64-bit Games</h1>
 <button on:click={() => games = []}>Clear List</button>
+  <button on:click={() => games = Object.values(gamesData['64bit']['genre']).flatMap(genre => genre.games)}>All Games</button>
   <button on:click={() => games = gamesData['64bit']['genre'].racing.games}>Racing</button>
   <button on:click={() => games = gamesData['64bit']['genre'].sport.games}>Sport</button>
 
-    <ul>
+  <ul>
     {#each games as game}
       <li>{game.title} - {game.platform}</li>
     {/each}
