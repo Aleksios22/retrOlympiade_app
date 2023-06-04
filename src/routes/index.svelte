057cd77ab547cabec25110d82a18262371e9f7e1
Aleksios22 <alexisrochon98@outlook.com>
2023-06-04 16:51:38 -0400

    1 file modified, 11 deleted

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,14 @@
 <script>
     import gamesData from '../data/games.json';
-    let n64games = gamesData['64bit']['genre'];
+    let n64games = gamesData;
 </script>
 
 <main>
 <h1>64-bit Games</h1>
 
-<button >Select Random Game</button>
- <button on:click={() => n64games = []}>Clear List</button>
-  <button on:click={() => n64games = gamesData['64bit']['genre'].racing.games}>Racing</button>
-  <button on:click={() => n64games = gamesData['64bit']['genre'].sport.games}>Sport</button>
-
     <ul>
-    {#each n64games as game}
-      <li>{game.title} - {game.platform}</li>
-    {/each}
+      <li>{n64games}</li>
+
   </ul>
 
 
